b1dbbdff717061d8e2f9e483183c0bdb61f84640
Rahul Yadav <yrahul9452@gmail.com>
2022-04-01 10:48:52 +0530

    pused

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = () => {
 
     return mongoose.connect(
-        "mongodb+srv://yadav9452:1234@cluster0.zup5e.mongodb.net/pagination?retryWrites=true&w=majority");
+        "mongodb+srv://yadav9452:1234@cluster0.7vr25.mongodb.net/Cluster0?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
